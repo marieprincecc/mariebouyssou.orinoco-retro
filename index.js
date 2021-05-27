dda2256e2c7ddd238c3775b4f5ba0dac9d6b1d0b
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-05-27 16:41:09 +0200

    fonctions

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-function imgProduit(){
+function imgProduit(i){
 fetch("http://localhost:3000/api/cameras") //appel des données a l'api
-.then(function data(res) {
+.then(function(res) {
     if (res.ok) {
         return res.json();
     }
 })
-.then(function data(value) {
-    console.log(value[0].imageUrl);
+.then(function(value) {
+    console.log(value[i].imageUrl);
 })
-.catch(function data(err) {
+.catch(function(err) {
     console.log("une erreure est survenue"); //reponse en cas d'erreur
 })
 }
-imgProduit()
 
+function descriptionProduit(i) {
+    fetch("http://localhost:3000/api/cameras") //appel des données a l'api
+.then(function(res) {
+    if (res.ok) {
+        return res.json();
+    }
+})
+.then(function(value) {
+    console.log(value[i].description);
+})
+.catch(function(err) {
+    console.log("une erreure est survenue"); //reponse en cas d'erreur
+})
+}
+
+function nameProduit(i) {
+    fetch("http://localhost:3000/api/cameras") //appel des données a l'api
+.then(function(res) {
+    if (res.ok) {
+        return res.json();
+    }
+})
+.then(function(value) {
+    console.log(value[i].name);
+})
+.catch(function(err) {
+    console.log("une erreure est survenue"); //reponse en cas d'erreur
+})
+}
+
+function priceProduit(i) {
+    fetch("http://localhost:3000/api/cameras") //appel des données a l'api
+.then(function(res) {
+    if (res.ok) {
+        return res.json();
+    }
+})
+.then(function(value) {
+    console.log(value[i].price);
+})
+.catch(function(err) {
+    console.log("une erreure est survenue"); //reponse en cas d'erreur
+})
+}
+
+priceProduit(0)
+nameProduit(0)
+descriptionProduit(0)
+imgProduit(0)
