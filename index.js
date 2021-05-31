6022f991ada1e2de3e5d9bcbffc40996d891565e
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-05-31 19:08:52 +0200

    debut fonction image (sans la boucle)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function produit(){
+function produit(i){
 fetch("http://localhost:3000/api/cameras") //appel des données a l'api
 .then(function(res) {
     if (res.ok) {
@@ -6,10 +6,25 @@ fetch("http://localhost:3000/api/cameras") //appel des données a l'api
     }
 })
 .then(function(value) {
-    console.log(value);
+    console.log(value)
+    let url= value[i].imageUrl;
+    let imgCam = document.getElementById("appareil"+i);
+    imgCam.setAttribute("src",url);
 })
 .catch(function(err) {
     console.log("une erreure est survenue"); //reponse en cas d'erreur
 })
 }
 
+const elt= () =>{
+    for (let i=0; i<6; i++){
+        let row= document.getElementById("rowProduit");
+        let url= value[i].imageUrl;
+     
+       
+}} 
+    
+
+console.log(elt);
+
+produit();
