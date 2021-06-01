35a7e6fad673e3876d3d5c611ac544e8e8a406df
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-06-01 12:50:18 +0200

    ajout des element page home

--- index.js
+++ index.js
@@ -7,31 +7,65 @@ fetch("http://localhost:3000/api/cameras") //appel des données a l'api
 })
 .then(function(value) {
     console.log(value)
-    let url= value[i].imageUrl;
-    let imgCam = document.getElementById("appareil"+i);
-    imgCam.setAttribute("src",url);
+let url= value[i].imageUrl;
+let nomCam= value[i].name;
+let prixProduit= value[i].price;
+
+console.log(url, nomCam,prixProduit);
+
+let imgCam = document.getElementById("appareil"+i);
+imgCam.setAttribute("src",url);
+    
+   
+   
+
+
+  
+  let titre = document.getElementById("titre"+i);
+  titre.innerHTML += nomCam; 
+ console.log(nomCam);
+
+
+
+    
+ 
+    let prix = document.getElementById("prix"+i);
+    prix.innerHTML += prixProduit+"€";
+
 })
+
+
 .catch(function(err) {
     console.log("une erreure est survenue"); //reponse en cas d'erreur
-})
-}
+    console.log(err);
+})}
+
+
+
+
+
+
+
+
 
 const elt= () =>{
     for (let i=0; i<6; i++){
         let row= document.getElementById("rowProduit");
-        let url= value[i].imageUrl;
+       
      
        
 }} 
-   produit(); 
-   
+
+
+
+
+
+
 const toutElt=()=>{
     for (let i = 0; i < 5; i++) {
-       
-produit(i)
-        
+        produit(i)
     }
 }
-console.log(elt);
+
 
 toutElt();
