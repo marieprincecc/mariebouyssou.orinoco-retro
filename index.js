7b5df8c43e4472457992b85a68264083bfeb386d
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-06-02 18:11:24 +0200

    page homme fonction ok

--- index.js
+++ index.js
@@ -48,74 +48,89 @@ const toutElt=()=>{                     //boucle produit
     }
 }
 
-const createLodgingCardTitle=()=>{
-    for (let i=0; i<6; i++){
-    const cardTitle=document.createElement('h4');
-    cardTitle.classList.add('card-title');
-    cardTitle.id="titre"+i;
+const createLodgingCardTitle=(i)=>{
+ 
+    let cardTitle=document.createElement("h4");
+    cardTitle.classList.add("card-title");
+    cardTitle.id=("titre"+i);
     return cardTitle;
-}}
-const createLodgingCardText=()=>{
-    for (let i=0; i<6; i++){
-    const cardText=document.createElement("p");
+}
+const createLodgingCardText=(i)=>{
+  
+    let cardText=document.createElement("p");
     cardText.classList.add("card-text");
-    cardText.id="prix"+i;
+    cardText.id=("prix"+i);
     return cardText;
-}}
-const createLodgingImg=()=>{
-    for (let i=0; i<6; i++){
-    const image= document.createElement('img');
-    image.classList.add('card-img-top');
-    image.id= "appareil"+i;
-    return image;
-}}
+}
+const createLodgingImg=(i)=>{
+    
+    let image= document.createElement("img");
+    image.classList.add("card-img-top");
+    
+        image.id = ("appareil"+i);
+    
+        return image;
+}
+const createLinkCard=()=>{
+    let pageProduit= ("html/produit.html");
+    let link= document.createElement("a");
+    link.classList.add("btn","btn-secondary","stretched-link")
+    link.setAttribute("href",pageProduit);
+    link.innerHTML="voir plus";
+    
+    return link;
+}
 
-const createLodgingCardBody=()=>{
-    const cardBody= document.createElement('div');
-    cardBody.classList.add("card-body"+"text-center");
+const createLodgingCardBody=(i)=>{
+    let cardBody= document.createElement("div");
+    cardBody.classList.add("card-body","text-center");
 
-    const cardTitle= createLodgingCardTitle();
+    let cardTitle= createLodgingCardTitle(i);
     cardBody.appendChild(cardTitle);
 
-    const cardText= createLodgingCardText();
+    let cardText= createLodgingCardText(i);
     cardBody.appendChild(cardText);
+
+    let link= createLinkCard();
+    cardBody.appendChild(link);
 
     return cardBody;
 }
 
-const createLodgingCard=()=>{
-    const card= document.createElement('card');
-    card.classList.add('card');
+const createLodgingCard=(i)=>{
+    let card= document.createElement("card");
+    card.classList.add("card");
 
-    const image= createLodgingImg();
+    let image= createLodgingImg(i);
     card.appendChild(image);
 
-    const cardBody= createLodgingCardBody();
+    let cardBody= createLodgingCardBody(i);
     card.appendChild(cardBody);
 
     return card;
 }
 
-const createLodgingCol=()=>{
-    const col= document.createElement('div');
-    col.classList.add("col-12"+"col-md-6"+"col-lg-4");
+const createLodgingCol=(i)=>{
+    let col= document.createElement("div");
+    col.classList.add("col-12","col-md-6","col-lg-4");
 
-    const card= createLodgingCard();
+    const card= createLodgingCard(i);
     col.appendChild(card);
     return col;
 }
-const insereCol=()=>{
+const insereCol=(i)=>{
     let row= document.getElementById("rowProduit");
-    const col= createLodgingCol();
+    let col= createLodgingCol(i);
     row.appendChild(col);
 }
 
 const elt= () =>{ 
       // crée le html en fonction du nombre d'article
-for (let i=0; i<6; i++){
+for (let i=0; i<5; i++){
+   
    insereCol(i)
+
 }}
 
 elt();
-
 toutElt();      //execute toutElt
