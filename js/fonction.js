368c29f67dadeab90c2ff05b71d637f7ae20141c
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-06-09 15:43:03 +0200

    mise au propre + commentaire produit.js

--- js/fonction.js
+++ js/fonction.js
@@ -1,27 +1,27 @@
-const produitImg= (value, i) =>{            //affiche les images 
+const produitImg= (value, i) =>{            //attribut l'url de l'image dans l'element #appareil +i correspondant
     let url= value[i].imageUrl;
     let imgCam = document.getElementById("appareil"+i);
     imgCam.setAttribute("src",url);
 }
 
-const produitNom= (value,i) =>{             //affiche le nom
+const produitNom= (value,i) =>{             //attribut le nom de la camera dans l'element #titre +i correspondant
     let nomCam= value[i].name;  
     let titre = document.getElementById("titre"+i);
     titre.innerHTML += nomCam; 
 }
 
-const produitPrice= (value,i) =>{              //affiche le prix
+const produitPrice= (value,i) =>{              //affiche le prix  dans l'element #prix +i correspondant
     let prixProduit= value[i].price;
     let prix = document.getElementById("prix"+i);
     prix.innerHTML += prixProduit+"€";
 }
 
-const produitDescription=(value,i)=>{            //affiche la description
+const produitDescription=(value,i)=>{            //affiche la description  dans l'element #description +i correspondant
     let descriptionProduit= value[i].description;
     let description = document.getElementById("description"+i);
     description.innerHTML += descriptionProduit;
 }
-const produitLense= (value,i) =>{                      //affiche l'array des personnalisations
+const produitLense= (value,i) =>{                      //affiche l'array des personnalisations dans l'element #persoProduit +i correspondant
     let lenseProduit= value[i].lenses;
     let lenses =document.getElementById("persoProduit"+i);
     lenses.innerHTML += "Modèles disponibles: "+lenseProduit;
--- js/produit.js
+++ js/produit.js
@@ -1,72 +1,57 @@
-
+const produitSelect=()=>{
    
-function produitSelect(){
-   
-    let params = new URLSearchParams(document.location.search.substring(1));
-    let id = params.get("id")
-    console.log(id);
+    let params = new URLSearchParams(document.location.search.substring(1)); //récupère search dans l'url
+    let id = params.get("id")                      //extrait l'id de l'url
+        console.log(id);
    
     fetch("http://localhost:3000/api/cameras/"+id) //appel des données a l'api
     
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function(value) {
-        console.log(value)
-       
-        
+        .then(function(res) {
+                if (res.ok) {
+                    return res.json();              //convertie les données json
+                }
+        })
+        .then(function(value) {
+             console.log(value)                         
+            produitImgSelect(value)          //execution des fonctions
+            produitNomSelect(value)
+            produitPriceSelect(value)
+            produitDescriptionSelect(value)
+            produitLenseSelect(value)
+        })
+    
+        .catch(function(err) {
+            console.log("une erreure est survenue"); //reponse en cas d'erreur
+            console.log(err);                          //affiche l'erreure dans la console
+        })
+}
+const produitImgSelect= (value) =>{            //attribut l'url de l'image dans l'element #appareil 
     let url= value.imageUrl;
-    let nomCam= value.name;
+    let imgCam = document.getElementById("appareil");
+    imgCam.setAttribute("src",url);
+}
+    
+const produitNomSelect= (value) =>{             //attribut le nom de la camera dans l'element #titre 
+    let nomCam= value.name;  
+    let titre = document.getElementById("titre");
+    titre.innerHTML += nomCam; 
+}
+    
+const produitPriceSelect= (value) =>{              //affiche le prix  dans l'element #prix
     let prixProduit= value.price;
+    let prix = document.getElementById("prix");
+    prix.innerHTML += prixProduit+"€";
+}
+    
+const produitDescriptionSelect=(value)=>{            //affiche la description  dans l'element #description
     let descriptionProduit= value.description;
+    let description = document.getElementById("description");
+     description.innerHTML += descriptionProduit;
+}
+const produitLenseSelect= (value) =>{                      //affiche l'array des personnalisations  dans l'element #persoProduit
     let lenseProduit= value.lenses;
-    let idProduit= value._id;
-    console.log(url, nomCam, prixProduit, descriptionProduit, lenseProduit,idProduit);
+    let lenses =document.getElementById("persoProduit");
+    lenses.innerHTML += "Modèles disponibles: "+lenseProduit;
+}
 
-   produitImgSelect(value)
-   produitNomSelect(value)
-   produitPriceSelect(value)
-   produitDescriptionSelect(value)
-   produitLenseSelect(value)
-  
-    
-                                            //execution des fonctions
-    
-    })
-    
-    .catch(function(err) {
-        console.log("une erreure est survenue"); //reponse en cas d'erreur
-        console.log(err);
-    })}
-    const produitImgSelect= (value) =>{            //affiche les images 
-        let url= value.imageUrl;
-        let imgCam = document.getElementById("appareil");
-        imgCam.setAttribute("src",url);
-    }
-    
-    const produitNomSelect= (value) =>{             //affiche le nom
-        let nomCam= value.name;  
-        let titre = document.getElementById("titre");
-        titre.innerHTML += nomCam; 
-    }
-    
-    const produitPriceSelect= (value) =>{              //affiche le prix
-        let prixProduit= value.price;
-        let prix = document.getElementById("prix");
-        prix.innerHTML += prixProduit+"€";
-    }
-    
-    const produitDescriptionSelect=(value)=>{            //affiche la description
-        let descriptionProduit= value.description;
-        let description = document.getElementById("description");
-        description.innerHTML += descriptionProduit;
-    }
-    const produitLenseSelect= (value) =>{                      //affiche l'array des personnalisations
-        let lenseProduit= value.lenses;
-        let lenses =document.getElementById("persoProduit");
-        lenses.innerHTML += "Modèles disponibles: "+lenseProduit;
-    }
-
-produitSelect()
+produitSelect()                 //execute la fonction
