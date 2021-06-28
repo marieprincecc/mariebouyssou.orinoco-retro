e89a31c0606e87c9432c1e9655e05c47cfa2dcb7
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-06-28 11:47:42 +0200

    affichage page recap

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,6 +1,18 @@
 function confirmation() {
     const recap= JSON.parse(localStorage.getItem("recap"))
 console.log(recap);
+
+let idDeCommande= document.getElementById("id")
+let total= document.getElementById("total")
+//let contact= document.getElementById("contact")
+
+let id= recap.orderId
+//let total= recap.products.price
+//let adress= recap.contact
+console.log(id,total);
+idDeCommande.textContent += " " + id
+total.textContent += " " + total
+
 }
 confirmation()
 
